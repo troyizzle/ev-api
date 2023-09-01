b74268c8228f06751164c45cafb624d352d676bb
Troy Underwood <troyizzle@gmail.com>
2023-09-01 08:34:16 -0500

    feat: change render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake db:migrate
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
